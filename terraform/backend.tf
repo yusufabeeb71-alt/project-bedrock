b8c051b615b3de041291a4f06d4c9563689cd33f
yusufabeeb71 <yusufabeeb71@gmail.com>
2026-06-26 22:50:19 +0100

    fix(cicd): add proper terraform plan and apply workflows with OIDC

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "project-bedrock-tfstate-805711586344"
+    bucket = "project-bedrock-tfstate-093831816623"
     key    = "project-bedrock/terraform.tfstate"
     region = "us-east-1"
   }
